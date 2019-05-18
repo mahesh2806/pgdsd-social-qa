89432ecd63812d615cef99653337ebf5a76d8124
Ridvik Trivedi <ridvik@yahoo.co.in>
2019-05-18 21:27:05 +0530

    Chnages in Admin controller for delete user

--- Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
+++ Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
@@ -31,29 +31,4 @@ public class AdminController {
             return new ResponseEntity<>("You do not have rights to delete a user!", HttpStatus.UNAUTHORIZED);
         }
     }
-
-    @GetMapping("/api/admin/users/all")
-    public ResponseEntity<?> getAllUsers(HttpSession httpSession) {
-        if (httpSession.getAttribute("currUser") == null) {
-            return new ResponseEntity<>("Please Login first to access this endpoint!", HttpStatus.UNAUTHORIZED);
-        } else if (((User) httpSession.getAttribute("currUser")).getRole().equalsIgnoreCase("admin")) {
-            return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("You do not have rights to access all users!", HttpStatus.UNAUTHORIZED);
-        }
-    }
-
-    @PostMapping("/api/admin/category")
-    public ResponseEntity<?> createCategory(@RequestParam(value = "categoryTitle") String categoryTitle,
-                                              @RequestParam(value = "description") String categoryDescription,
-                                              HttpSession httpSession) {
-        if (httpSession.getAttribute("currUser") == null) {
-            return new ResponseEntity<>("Please Login first to access this endpoint!", HttpStatus.UNAUTHORIZED);
-        } else if (((User) httpSession.getAttribute("currUser")).getRole().equalsIgnoreCase("admin")) {
-            categoryService.addCategory(categoryTitle, categoryDescription);
-            return new ResponseEntity<>(categoryTitle + " category added successfully.", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("You do not have rights to add categories.", HttpStatus.UNAUTHORIZED);
-        }
-    }
 }
