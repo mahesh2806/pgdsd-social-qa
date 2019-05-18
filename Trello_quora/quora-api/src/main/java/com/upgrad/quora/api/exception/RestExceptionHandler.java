9c13fd5689f6ae16c9d2a97931c326ec933a3763
mahesh <maheshs@yupptv.com>
2019-05-18 22:45:46 +0530

    deprecated duplicate import statements

--- Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
+++ Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
@@ -2,9 +2,6 @@ package com.upgrad.quora.api.exception;
 
 
 import com.upgrad.quora.api.model.ErrorResponse;
-import com.upgrad.quora.service.exception.AuthenticationFailedException;
-import com.upgrad.quora.service.exception.SignUpRestrictedException;
-import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
