00b99b77182341acee294439ed8013cf5fa33870
Aman kumar <aman@Amans-MacBook-Air.local>
2019-05-19 21:39:57 +0530

    Added question related methods

--- Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -62,4 +62,107 @@ public class QuestionBusinessService {
         return questionEntityList;
     }
 
+/**
+     * This method performs the buisness logic required to edit question
+     *
+     * @param authorizationToken
+     * @param questionId
+     * @param questionContent
+     * @return
+     * @throws AuthorizationFailedException
+     * @throws InvalidQuestionException
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity performEditQuestionContent(final String authorizationToken,
+                                                     final String questionId, String questionContent)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        QuestionEntity questionEntity = new QuestionEntity();
+        UserAuthTokenEntity userAuthTokenEntity = userBusinessService.getUserAuthToken(authorizationToken);
+        if (userAuthTokenEntity != null) {
+            if (userBusinessService.isUserSignedIn(userAuthTokenEntity)) {
+                questionEntity = questionDao.getUserForQuestionId(questionId);
+                if (questionEntity != null) {
+                    if (isUserQuestionOwner(userAuthTokenEntity.getUser(), questionEntity.getUser())) {
+                        questionEntity.setContent(questionContent);
+                        questionDao.updateQuestion(questionEntity);
+                    } else {
+                        throw new AuthorizationFailedException("ATHR-003", "Only the question owner can edit the question");
+                    }
+                } else {
+                    throw new InvalidQuestionException("QUES-001", "Entered question uuid does not exist");
+                }
+            } else {
+                throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to edit the question");
+            }
+        } else {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+        return questionEntity;
+    }
+
+    /**
+     * This method performs the business logic required to delete question
+     *
+     * @param authorizationToken
+     * @param questionId
+     * @throws AuthorizationFailedException
+     * @throws InvalidQuestionException
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void performDeleteQuestion(final String authorizationToken,
+                                      final String questionId)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        UserAuthTokenEntity userAuthTokenEntity = userBusinessService.getUserAuthToken(authorizationToken);
+        if (userAuthTokenEntity != null) {
+            if (userBusinessService.isUserSignedIn(userAuthTokenEntity)) {
+                QuestionEntity questionEntity = questionDao.getUserForQuestionId(questionId);
+                if (questionEntity != null) {
+                    if (isUserQuestionOwner(userAuthTokenEntity.getUser(), questionEntity.getUser())
+                            || userBusinessService.isUserAdmin(userAuthTokenEntity.getUser())) {
+                        questionDao.deleteQuestion(questionEntity);
+                    } else {
+                        throw new AuthorizationFailedException("ATHR-003", "Only the question owner or admin can delete the question");
+                    }
+                } else {
+                    throw new InvalidQuestionException("QUES-001", "Entered question uuid does not exist");
+                }
+            } else {
+                throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to delete a question");
+            }
+        } else {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+    }
+
+    /**
+     * This method performs the business logic required to retrieve questions bya user
+     *
+     * @param authorizationToken
+     * @param userUuId
+     * @return
+     * @throws AuthorizationFailedException
+     * @throws UserNotFoundException
+     */
+    public List<QuestionEntity> performGetAllQuestionsByUser(final String authorizationToken,
+                                                             @PathVariable("userId") final String userUuId) throws AuthorizationFailedException,
+            UserNotFoundException {
+        List<QuestionEntity> questionEntityList = new ArrayList<QuestionEntity>();
+        UserAuthTokenEntity userAuthTokenEntity = userBusinessService.getUserAuthToken(authorizationToken);
+        if (userAuthTokenEntity != null) {
+            if (userBusinessService.isUserSignedIn(userAuthTokenEntity)) {
+                UserEntity userEntity = userDao.getUser(userUuId);
+                if (userEntity != null) {
+                    questionEntityList = questionDao.getQuestionsForUserId(userEntity.getId());
+                } else {
+                    throw new UserNotFoundException("USR-001", "User with entered uuid whose question details are to be seen does not exist");
+                }
+            } else {
+                throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get all questions posted by a specific user");
+            }
+        } else {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+        return questionEntityList;
+    }
+
 }
